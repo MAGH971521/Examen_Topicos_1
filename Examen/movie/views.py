4c8302acf576d06ca859e88f7fb15b0718132176
MAGH971521 <miguel.galvan17@tectijuana.edu.mx>
2018-04-12 22:53:22 -0700

    Rebuild

--- Examen/movie/views.py
+++ Examen/movie/views.py
@@ -3,6 +3,10 @@ import redis, ast, json
 from .models import Movie
 
 # Create your views here.
+
+def home(request):
+    return render(request, "index.html")
+
 def sql_to_redis(request):
     red = redis.Redis(host='localhost', port=9090, db=0)
     structure = {
@@ -18,5 +22,8 @@ def sql_to_redis(request):
             "active": val.active,
             "created": val.created
         }})
-    red.hmset("FakeDB", structure)
-    return redirect()
+
+        red.sadd("MyDB:Movies:" + val.id, structure["Movies"][val.id])
+    data_value = list(red.smember("MyDB:Movies:{}".format(val.id)))
+    data_value[0] = data_value[0].decode('utf-8')
+    return render(request, "index.html", {'data_value': data_value})
